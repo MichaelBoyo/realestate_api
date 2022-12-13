d5e780abbbeb00430f9098e74e5a70cf9739a917
MichaelBoyo <boyomichaelbidemi@gmail.com>
2022-12-13 17:27:09 +0000

    ok

--- lands/views.py
+++ lands/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 
 from lands.models import Land
@@ -7,3 +8,7 @@ from lands.serializers import LandSerializer
 class LandViewSet(viewsets.ModelViewSet):
     queryset = Land.objects.all()
     serializer_class = LandSerializer
+
+
+def index(request):
+    return render(request, 'lands/index.html')
--- realestate_api/urls.py
+++ realestate_api/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from lands.views import LandViewSet
+from lands.views import LandViewSet, index
 
 router = DefaultRouter()
 router.register(r'lands', LandViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls'))
 ]
